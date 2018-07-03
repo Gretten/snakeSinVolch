023e29cdadd1269fdb5b065247331c178b2d39d0
MaxBlazer <maxiblazer@gmail.com>
2018-07-03 21:50:54 +0300

    added js draft

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-var snake - 'shhhh';
+// vars
+// snake_array
+// snake_head
+// snake_direction
+// food_pos
+
+// while true
+	// if key pressed
+		// change direction
+	// step
+	// draw
+	// pause
+
+// draw func
+	// field
+		// for
+			// for
+			
+	// food
+
+	// for
+		// snake
+
+// change func
