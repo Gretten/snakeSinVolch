9bba0f0e715bd0cad3682efcc2e3e56498d0bed0
MaxBlazer <maxiblazer@gmail.com>
2018-07-03 22:44:21 +0300

    fixed js filename

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-// volching is not allowed there
 // game vars
-// field sizes (height/width)
-// snake_array
-// snake_head
-// snake_direction
-// food_pos
+var
+	field_size = [15, 15];
+	// snake_array
+	// snake_head
+	// snake_direction
+	// food_pos
 
 // graphic vars
-// cell size (visible width)
-// colors...
+	// cell size (visible width)
+	// colors...
+
+setInterval(draw, 1000);
 
 // while true
 	// if key pressed
@@ -17,22 +19,24 @@
 	// draw
 	// pause
 
-// draw func
 function draw() {
   var canvas = document.getElementById('canvas');
+  
   if (canvas.getContext) {
+	// ctx.clearRect(45,45,60,60);
+	// ctx.strokeRect(50,50,50,50);
+	console.log("canvas is successed found");
+  
     var ctx = canvas.getContext('2d');
 
-    ctx.fillRect(25,25,100,100);
-    ctx.clearRect(45,45,60,60);
-    ctx.strokeRect(50,50,50,50);
-  }
-}
 	// field
 		// for
 			// for
 			
 	// food
+	ctx.fillRect(25, 25, 100, 100);
 
 	// for
 		// snake
+  }
+}
